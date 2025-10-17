680d57e2bb37e8ccbf03057575936a6b43a8656b
chaima askri <askrichayma7@gmail.com>
2025-10-17 16:55:17 +0200

    Troiisième commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        //printh Hello, World! to the console
+        // Added comment for clarity
         System.out.println("Hello, World!");
     }
 }
